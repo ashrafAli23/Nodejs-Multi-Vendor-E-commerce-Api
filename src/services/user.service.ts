6d00fd53053f8b7cb8a04ae483c884ca3478e202
ashrafAli23 <ashrafammar230@gmail.com>
2022-07-19 10:04:10 +0200

    update user services

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 import UserModel, { IUser } from "../models/User";
 import { ErrorResponse } from "../apiResponse/errorResponse";
 import { NotFoundError } from "../apiResponse/notFoundError";
+import { Request } from "express";
 
 /**
  * @docs    Create New User
@@ -23,6 +24,72 @@ const createUser = async (body: IUser): Promise<IUser> => {
 };
 
 /**
+ * @desc    Update User Details Using It's ID
+ * @param   { String } userId - An string from logged in user data
+ * @param   { Object } body - Body object data
+ * @returns { Object<user> }
+ */
+const updateUser = async (userId: string, body: any): Promise<IUser> => {
+  const user = await UserModel.findByIdAndUpdate({ _id: userId }, body, {
+    new: true,
+  });
+
+  if (!user) {
+    throw new ErrorResponse("User update failed");
+  }
+
+  return user;
+};
+
+/**
+ * @desc    Update User Password Using It's ID
+ * @param   { String } userId - An string from logged in user data
+ * @param   { Object } body - Body object data
+ * @returns { Object<user> }
+ */
+const updateUserPassword = async (
+  userId: string,
+  body: any
+): Promise<IUser> => {
+  const { newPassword, oldPassword } = body;
+
+  if (newPassword === oldPassword) {
+    throw new ErrorResponse("Old password can't be same with new password");
+  }
+
+  const user = await UserModel.findByIdAndUpdate(
+    userId,
+    {
+      password: newPassword,
+    },
+    { new: true }
+  );
+
+  if (!user) {
+    throw new NotFoundError("User not found");
+  }
+
+  return user;
+};
+
+/**
+ * @desc    Update User Photo Using It's ID
+ * @param   { String } userId - An string from logged in user data
+ * @param   { File } image - File data
+ */
+const UserProfileImage = async (userId: string, image: any) => {
+  if (!image) {
+    throw new ErrorResponse("Profile Image Required");
+  }
+
+  await UserModel.findByIdAndUpdate(
+    userId,
+    { photo: image.path },
+    { new: true }
+  );
+};
+
+/**
  * @docs    Find User By ID
  * @param   { Object } userId - Body user data
  * @returns { Object<user> }
@@ -38,4 +105,7 @@ const findUserById = async (userId: string): Promise<IUser> => {
 export default {
   createUser,
   findUserById,
+  updateUser,
+  updateUserPassword,
+  UserProfileImage,
 };
