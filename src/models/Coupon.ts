5ed4f3e87a7c058b31bf45a5a3aad204cb79b80c
ashrafAli23 <ashrafammar230@gmail.com>
2022-07-23 10:11:55 +0200

    update coupon model

--- src/models/Coupon.ts
+++ src/models/Coupon.ts
@@ -1,13 +1,9 @@
 import { model, Schema, Document, Types } from "mongoose";
 
-interface ICoupon extends Document {
+export interface ICoupon extends Document {
   code: string;
-  title: string;
-  expire: Date;
-  usageLimit: number;
   percentageDiscount: number;
   maxCouponAmount: number;
-  createdBy?: string;
 }
 
 const couponSchema = new Schema({
@@ -15,26 +11,14 @@ const couponSchema = new Schema({
     type: String,
     required: true,
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  expire: {
-    type: Date,
-    required: true,
-  },
-  usageLimit: {
-    type: Number,
-    required: true,
-  },
   percentageDiscount: {
     type: Number,
     required: true,
   },
-  createdBy: {
-    type: Types.ObjectId,
-    ref: "Users",
-  },
+  maxCouponAmount:{
+	type: Number,
+    required: true,
+  }
 });
 
 export default model<ICoupon>("Coupon", couponSchema);
